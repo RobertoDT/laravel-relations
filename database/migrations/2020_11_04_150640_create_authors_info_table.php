f61c8a465fdf2993b8062111b69e974a5e6fc71e
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-11-04 16:27:30 +0100

    aggiunta tabella autore, tabella info autore e creati i seeder

---
+++ database/migrations/2020_11_04_150640_create_authors_info_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors_info', function (Blueprint $table) {
+            $table->foreignId('author_id')->constrained();
+            $table->string("nationality", 50);
+            $table->text("biography");
+            $table->string("image")->default("http://via.placeholder.com/200x300");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors_info');
+    }
+}
